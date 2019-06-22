031203370196f3a6ef658cd9527dc6d356886879
Matt Croxson <matt.croxson@carsales.com.au>
2019-06-22 17:17:29 +1000

    Initial Commit

---
+++ HelperKit/HelperKit.h
@@ -0,0 +1,19 @@
+//
+//  HelperKit.h
+//  HelperKit
+//
+//  Created by Matt Croxson on 22/6/19.
+//  Copyright © 2019 Matt Croxson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HelperKit.
+FOUNDATION_EXPORT double HelperKitVersionNumber;
+
+//! Project version string for HelperKit.
+FOUNDATION_EXPORT const unsigned char HelperKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelperKit/PublicHeader.h>
+
+
