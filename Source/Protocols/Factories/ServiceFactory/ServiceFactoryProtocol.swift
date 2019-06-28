7d4f964d090b62de7359daeabdc0bf14135acf5a
Matt Croxson <mattcroxson@gmail.com>
2019-06-29 09:50:18 +1000

    Added ServiceFactoryProtocol to conglomerate factories

---
+++ Source/Protocols/Factories/ServiceFactory/ServiceFactoryProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  ServiceFactoryProtocol.swift
+//  HelperKit
+//
+//  Created by Matt Croxson on 29/6/19.
+//  Copyright © 2019 Matt Croxson. All rights reserved.
+//
+
+import Foundation
+
+public protocol ServiceFactoryProtocol {
+    var viewControllerFactory: ViewControllerFactoryProtocol { get }
+    var coordinatorFactory: CoordinatorFactoryProtocol { get }
+}
