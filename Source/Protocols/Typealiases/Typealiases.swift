bcb28275ded32529045bf849b8b7c3dbfb869ffa
Matt Croxson <mattcroxson@gmail.com>
2019-06-29 18:03:04 +1000

    Added typealiases

---
+++ Source/Protocols/Typealiases/Typealiases.swift
@@ -0,0 +1,9 @@
+//
+//  Typealiases.swift
+//  HelperKit
+//
+//  Created by Matt Croxson on 29/6/19.
+//  Copyright © 2019 Matt Croxson. All rights reserved.
+//
+
+public typealias TextEntryUpdateHandler = (_ newValue: String?) -> Void
