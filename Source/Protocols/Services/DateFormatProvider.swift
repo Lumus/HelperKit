4d0a9fbeaeeec97a7d0d14143392e0f757cba2df
Matt Croxson <mattcroxson@gmail.com>
2019-07-31 20:41:39 +1000

    Updated protocol to provide additional date formatting options.

--- Source/Protocols/Services/DateFormatProvider.swift
+++ Source/Protocols/Services/DateFormatProvider.swift
@@ -10,7 +10,18 @@ import Foundation
 
 public protocol DateFormatProvider {
 
-    /// Returns a formatterd string for the provided date.
+    /// Returns a formatted string for the provided date.
     /// - Parameter from: Date to get string for.
+    /// - Returns: Formatted string.
     func string(from: Date) -> String
+
+    /// Returns a Date from an ISO8601 formatted string.
+    /// - Parameter iso8601String: ISO8601 formatted string.
+    /// - Returns: Date, or nil if the ISO8601 string cannot be decoded.
+    func date(from iso8601String: String) -> Date?
+
+    /// Returns a formatted string for the provided ISO8601 formatted string.
+    /// - Parameter iso8601String: ISO8601 formatted string.
+    /// - Returns: Formatted string, or nil of the ISO8601 string cannot be decoded.
+    func string(from iso8601String: String) -> String?
 }
