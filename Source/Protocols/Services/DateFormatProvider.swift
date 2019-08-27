1cb32ff900c37c5a9b641455e23f10a1b8b5a695
Matt Croxson <mattcroxson@gmail.com>
2019-08-27 21:57:50 +1000

    Refactored date formatter to accept configuration

--- Source/Protocols/Services/DateFormatProvider.swift
+++ Source/Protocols/Services/DateFormatProvider.swift
@@ -12,16 +12,33 @@ public protocol DateFormatProvider {
 
     /// Returns a formatted string for the provided date.
     /// - Parameter from: Date to get string for.
-    /// - Returns: Formatted string.
-    func string(from: Date) -> String
+    /// - Parameter configuration: Configuration for the date formatter.
+    /// - Returns: Date to get string for.
+    func string(from: Date,
+                configuration: DateFormatProviderConfiguration) -> String
+
+    /// Returns a formatted string for the provided ISO8601 formatted string.
+    /// - Parameter iso8601String: ISO8601 formatted string.
+    /// - Parameter configuration: Configuration for the date formatter.
+    /// - Returns: Formatted string, or nil if the ISO8601 string cannot be decoded.
+    func string(from iso8601String: String?,
+                configuration: DateFormatProviderConfiguration) -> String?
 
     /// Returns a Date from an ISO8601 formatted string.
     /// - Parameter iso8601String: ISO8601 formatted string.
     /// - Returns: Date, or nil if the ISO8601 string cannot be decoded.
     func date(from iso8601String: String?) -> Date?
 
-    /// Returns a formatted string for the provided ISO8601 formatted string.
-    /// - Parameter iso8601String: ISO8601 formatted string.
-    /// - Returns: Formatted string, or nil of the ISO8601 string cannot be decoded.
-    func string(from iso8601String: String?) -> String?
+}
+
+public struct DateFormatProviderConfiguration {
+    public static let `default` = DateFormatProviderConfiguration(dateStyle: .medium, timeStyle: .short)
+
+    public let dateStyle: DateFormatter.Style
+    public let timeStyle: DateFormatter.Style
+
+    public init(dateStyle: DateFormatter.Style, timeStyle: DateFormatter.Style) {
+        self.dateStyle = dateStyle
+        self.timeStyle = timeStyle
+    }
 }
